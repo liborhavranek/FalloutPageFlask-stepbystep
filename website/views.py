e60b1e8464b7f9a34f66b42452b8d704d49ff206
liborhavranek <liborhavranek91@gmail.com>
2022-12-10 00:17:33 +0100

    login a register are created database working good

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 # File for everything what is visible for everyone 
 from flask import Blueprint, render_template
+from flask_login import login_user, logout_user, login_required, current_user
 
 views = Blueprint('views', __name__)
 
