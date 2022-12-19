a17b8d4f07b96e7796d8e4602388131712f80d29
liborhavranek <liborhavranek91@gmail.com>
2022-12-19 11:06:12 +0100

    upraveny dokumenty

--- website/__init__.py
+++ website/__init__.py
@@ -5,48 +5,40 @@ from flask_login import LoginManager
 from flask_ckeditor import CKEditor
 
 
-
 db = SQLAlchemy()
-DB_NAME="database.db"
-
-
+DB_NAME = "database.db"
 
 
 def create_app():
-    app = Flask(__name__) 
-    app.config['SECRET_KEY']= 'key'
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'key'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-    
-    
+
     UPLOAD_FOLDER = 'website/static/images/uploads'
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-    
 
-    
     from .views import views
     from .auth import auth
-    
+
     app.register_blueprint(views, url_prefix=('/'))
     app.register_blueprint(auth, url_prefix=('/'))
 
-
-    
     from .models import User, Post, Comment, Like, Dislike
     create_database(app)
-    
+
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-    
+
     ckeditor = CKEditor(app)
-    
-    
+
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
 
     return app
+
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
