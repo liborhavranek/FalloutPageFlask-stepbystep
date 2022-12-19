af84847b4eed25a9ebc89bef106916d6cfacdacb
liborhavranek <liborhavranek91@gmail.com>
2022-12-19 13:43:48 +0100

    again upload secret key

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 from flask_ckeditor import CKEditor
-
+from creds import my_secret_key
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -11,7 +11,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'key'
+    app.config['SECRET_KEY'] = my_secret_key
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
